cdf6ce043fcaacc10deaac8e6ab912d1afc39224
Railson Frazão <railsonpontes4@gmail.com>
2020-12-03 08:44:13 -0300

    Update

--- inicio.sh
+++ inicio.sh
@@ -7,12 +7,15 @@ while :; do
 	read -p "Digite a opcao  >>" opcao
 	case $opcao in
 		1) ls;;
-		2) ;;
+		2) read -p "Digite o diretorio de destino >>" dr
+		cd $dr
+		echo "Você está aqui >>" $(pwd);;
 		3) read -p "Digite o filtro >>" aux
 		ls | grep -E $aux;;
 		4) ls -l;;
 		5) ;;
-		6) ;;
+		6) read -p "Digite o nome do aquivo que quer ver:" arq
+		cat $arq;;
 		7) ;;
 		8) ;;
 		*) break ;;
